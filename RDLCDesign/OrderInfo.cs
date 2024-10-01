83aa738e89c5d1a660eb44cdd19f28f9116c2cfd
rezwan <rezwanshahriorrahin@gmail.com>
2024-10-01 13:47:55 +0600

    create Windows Form App as 'RDLCDesign'

---
+++ RDLCDesign/OrderInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RDLCDesign
+{
+	internal class OrderInfo
+	{
+		public int Id { get; set; }
+        public string Item { get; set; }
+        public string Customer { get; set; }
+        public DateTime Date { get; set; }
+		public int Quantity { get; set; }
+		public decimal NetCost { get; set; }
+	}
+}
